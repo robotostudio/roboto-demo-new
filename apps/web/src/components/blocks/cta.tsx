5d1f8e4ffc06eb6f893e792af42c9e2873547bbd
Jonathan Alford <jono@roboto.studio>
2024-03-26 16:32:43 +0000

    Update CtaBlockProps type with additional buttons property

--- apps/web/src/components/blocks/cta.tsx
+++ apps/web/src/components/blocks/cta.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 import { Cta, SanityKeyed } from '~/schema';
+import { SanityButtons } from '~/types';
 import Meteors from '../global/meteor';
 import { Buttons } from '../global/buttons';
 import { RichText } from '../global/richText';
 
-export type CtaBlockProps = SanityKeyed<Cta>;
+export type CtaBlockProps = SanityKeyed<Cta> & {
+  buttons: SanityButtons;
+};
 
 export const CtaBlock: FC<CtaBlockProps> = ({ title, richText, buttons }) => {
   return (
@@ -15,7 +18,7 @@ export const CtaBlock: FC<CtaBlockProps> = ({ title, richText, buttons }) => {
           <h2 className="mb-4 text-3xl font-bold text-white sm:text-4xl">
             {title}
           </h2>
-          <div className="py-4">
+          <div className="py-4 text-white text-opacity-80">
             <RichText value={richText} />
           </div>
           <Buttons
