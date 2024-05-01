3aa2e62e548bbb89f3ab50cfda080b1ad2f2d097
hrithikroboto <hrithik@roboto.studio>
2024-05-01 11:36:53 +0530

    created webhook route

--- apps/web/src/config/constant.ts
+++ apps/web/src/config/constant.ts
@@ -19,6 +19,54 @@ export const SANITY_TAGS = {
   navbar: 'navbar',
 } as const;
 
+export const getSanityTags = (type: string, locale?: Locale) => {
+  const isLocalized = !!locale;
+
+  if (type === 'mainPage') {
+    return [
+      SANITY_TAGS.mainPage,
+      ...(isLocalized ? [LOCALIZED_SANITY_TAGS.mainPage(locale)] : []),
+    ];
+  }
+
+  if (type === 'page') {
+    return [
+      SANITY_TAGS.slugPage,
+      ...(isLocalized ? [LOCALIZED_SANITY_TAGS.slugPage(locale)] : []),
+    ];
+  }
+
+  if (type === 'blog') {
+    return [
+      SANITY_TAGS.blogs,
+      SANITY_TAGS.blogPage,
+      SANITY_TAGS.slugPage,
+      ...(isLocalized ? [LOCALIZED_SANITY_TAGS.blogPage(locale)] : []),
+    ];
+  }
+
+  if (type === 'blogIndex') {
+    return [
+      SANITY_TAGS.blogIndex,
+      ...(isLocalized
+        ? [
+            LOCALIZED_SANITY_TAGS.blogIndex(locale),
+            LOCALIZED_SANITY_TAGS.blogs(locale),
+          ]
+        : []),
+    ];
+  }
+
+  if (type === 'footer') {
+    return [SANITY_TAGS.footer];
+  }
+  if (type === 'navbar') {
+    return [SANITY_TAGS.navbar];
+  }
+
+  return [];
+};
+
 export const LOCALIZED_SANITY_TAGS = {
   mainPage: (locale: Locale) => `${SANITY_TAGS.mainPage}-${locale}`,
   blogPage: (locale: Locale) => `${SANITY_TAGS.blogPage}-${locale}`,
