a0c2067b542202329055d4ad2faf970f2b42416e
hrithikroboto <hrithik@roboto.studio>
2024-05-01 13:32:55 +0530

    body log

--- apps/web/src/app/api/reavalidate/route.ts
+++ apps/web/src/app/api/reavalidate/route.ts
@@ -14,16 +14,16 @@ type WebhookPayload = {
 
 export async function POST(req: NextRequest) {
   try {
-      console.log(
-        '🚀 ~ POST ~ body:',
-        req.headers.get('sanity-webhook-signature'),
-        process.env.SANITY_REVALIDATE_SECRET,
-      );
       const { isValidSignature, body } = await parseBody<WebhookPayload>(
         req,
         process.env.SANITY_REVALIDATE_SECRET,
       );
-
+      console.log(
+        '🚀 ~ POST ~ body:',
+        req.headers.get('sanity-webhook-signature'),
+        process.env.SANITY_REVALIDATE_SECRET,
+        body,
+      );
 
     if (!isValidSignature) {
       const message = 'Invalid signature';
