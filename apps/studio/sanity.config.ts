1cfb0bde202580f1b5926f296658946af7a162aa
hrithikroboto <hrithik@roboto.studio>
2024-05-07 13:02:57 +0530

    removed default intital template on internalized docs

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'sanity';
-import { structureTool } from 'sanity/structure';
-import { visionTool } from '@sanity/vision';
-import { schemaTypes } from './schemaTypes';
-import { defaultDocumentNode, structure } from './structure';
 import { assist } from '@sanity/assist';
 import { documentInternationalization } from '@sanity/document-internationalization';
-import { abTest } from 'sanity-plugin-roboto-ab-test';
-import { media } from 'sanity-plugin-media';
-import { iconPicker } from 'sanity-plugin-icon-picker';
-import { internationalizedDocuments } from './schemaTypes/documents';
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
 import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash';
+import { iconPicker } from 'sanity-plugin-icon-picker';
+import { media } from 'sanity-plugin-media';
+import { abTest } from 'sanity-plugin-roboto-ab-test';
+import { structureTool } from 'sanity/structure';
+import { schemaTypes } from './schemaTypes';
+import { internationalizedDocuments } from './schemaTypes/documents';
+import { defaultDocumentNode, structure } from './structure';
 import { abTestDocumentTypes } from './utils/abTest';
+import { getFlag } from './utils/helper';
 
 export default defineConfig({
   name: 'default',
@@ -52,5 +53,23 @@ export default defineConfig({
 
   schema: {
     types: schemaTypes,
+    templates: (prev) => {
+      const _internalizedDocs = internationalizedDocuments.map(
+        (doc) => `${doc}`,
+      );
+
+      const filtered = prev
+        .filter((template) => !_internalizedDocs.includes(template.id))
+        .map((template) => {
+          return {
+            ...template,
+            ...(template?.value?.language && {
+              title: `${getFlag(template.value.language)} ${template.title}`,
+            }),
+          };
+        });
+
+      return [...filtered];
+    },
   },
 });
